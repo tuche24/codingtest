84cf5722f007e4b913d6330929e06c5b706323a5
jeong <tuche24@naver.com>
2021-04-22 23:35:50 +0900

    실전4 게임개발 문제 푸는 중..

--- codingtest/src/chapter04/ch04_04.java
+++ codingtest/src/chapter04/ch04_04.java
@@ -1,13 +1,22 @@
 package chapter04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 // 실전 문제 게임 개발
 public class ch04_04 {
 
 	public static void main(String[] args) {
 		ch04_04 ch = new ch04_04();
-		ch.ch04_04_ex1();
+		try {
+			ch.ch04_04_ex2();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// 입력값
 		// 4 4
 		// 1 1 0
@@ -33,8 +42,8 @@ public class ch04_04 {
 		int mapY = Integer.parseInt(mapSizeArray[1]);
 		
 		String[] locationArray = location.split(" ");
-		int locationX = Integer.parseInt(locationArray[0]);
-		int locationY = Integer.parseInt(locationArray[1]);
+		int locationX = Integer.parseInt(locationArray[0]) - 1;
+		int locationY = Integer.parseInt(locationArray[1]) - 1;
 		int direction = Integer.parseInt(locationArray[2]);
 		
 		int[][] mapMatrix = new int[mapX][mapY];
@@ -66,11 +75,11 @@ public class ch04_04 {
 			int nx = locationX + dx[direction];
 			int ny = locationY + dy[direction];
 			
-			if(nx <= 0 || ny <= 0 || nx > mapX || ny > mapY) {
+			System.out.println("nx, ny location = " + nx + " " + ny + " count = " + count);
+			
+			if(nx < 0 || ny < 0 || nx > mapX - 1 || ny > mapY - 1) {
 				continue;
 			}
-			
-			System.out.println("nx, ny location = " + nx + " " + ny + " count = " + count);
 			
 			// 회전한 후 정면이 육지이고, 방문하지 않은 경우
 			if(mapMatrix[nx][ny] == 0 && visited[nx][ny] == 0) {
@@ -88,9 +97,11 @@ public class ch04_04 {
 				nx = locationX - dx[direction];
 				ny = locationY - dy[direction];
 				
-				if(mapMatrix[nx][ny] == 0) {
+				if(mapMatrix[nx][ny] == 0 && visited[nx][ny] == 0) {
+					visited[nx][ny] = 1;
 					locationX = nx;
 					locationY = ny;
+					turn_time = 0;
 				} else {
 					break;
 				}
@@ -112,4 +123,83 @@ public class ch04_04 {
 		return result;
 	}
 
+	// 문제가 안 풀려서 참조 https://velog.io/@uhan2/%EC%9D%B4%EA%B2%83%EC%9D%B4-%EC%BD%94%EB%94%A9-%ED%85%8C%EC%8A%A4%ED%8A%B8%EB%8B%A4.Part02.Chapter04-%EA%B2%8C%EC%9E%84-%EA%B0%9C%EB%B0%9C-Java
+	// 출력값이 정답과 다르다.
+	// StringTokenizer의 활용법을 익힐 수 있었음
+	public void ch04_04_ex2() throws IOException {
+		int[][] map;
+		boolean[][] visit;
+		int playerDir;
+		int playerRowPos;
+		int playerColPos;
+		// 북동남서를 바라볼때 움직이는 경로
+		int[] dRow = {-1, 0, 1, 0};
+		int[] dCol = {0, 1, 0, -1};
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int mapRow = Integer.parseInt(st.nextToken());
+		int mapCol = Integer.parseInt(st.nextToken());
+		
+		map = new int[mapRow][mapCol];
+		visit = new boolean[mapRow][mapCol];
+		
+		st = new StringTokenizer(br.readLine());
+		playerRowPos = Integer.parseInt(st.nextToken());
+		playerColPos = Integer.parseInt(st.nextToken());
+		playerDir = Integer.parseInt(st.nextToken());
+		
+		visit[playerRowPos][playerColPos] = true;
+		
+		for(int i = 0 ; i < mapRow; i++){
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0 ; j < mapCol; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int count = 0;
+		int visitCount = 1;
+		
+		while(true) {
+			turnPlayer(playerDir);
+			
+			if((map[playerRowPos + dRow[playerDir]][playerColPos + dCol[playerDir]] == 0) &&
+					(visit[playerRowPos + dRow[playerDir]][playerColPos + dCol[playerDir]] == false)) {
+				visit[playerRowPos + dRow[playerDir]][playerColPos + dCol[playerDir]] = true;
+				visitCount++;
+				
+				playerRowPos += dRow[playerDir];
+				playerColPos += dCol[playerDir];
+				
+				count = 0;
+			} else {
+				count++;
+			}
+			
+			if(count == 4) {
+				if(map[playerRowPos - dRow[playerDir]][playerColPos - dCol[playerDir]] == 1) {
+					break;
+				} else {
+					count = 0;
+					playerRowPos -= dRow[playerDir];
+					playerColPos -= dCol[playerDir];
+				}
+			}
+		}
+		
+		System.out.println(visitCount);
+		
+	}
+	
+	public int turnPlayer(int playerDir) {
+		playerDir -= 1;
+		
+		if (playerDir < 0) {
+			playerDir = 3;
+		}
+		
+		return playerDir;
+	}
 }
